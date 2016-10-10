37ec4704f9c5b432ff6ef386f06f279854fb10f3
RichardEitz <richard@leismunicipais.com.br>
2016-10-10 09:24:33 -0300

    Fix tests

--- test/lexical-test.js
+++ test/lexical-test.js
@@ -3,10 +3,10 @@
 const fs = require('fs')
 const assert = require('assert')
 
-const LexicalParser = require('../LexicalParser')
+const Lex = require('../Lex')
 const TokenMatcher = require('../TokenMatcher')
 
-describe('LexicalParser', function() {
+describe('Lex', function() {
 	let input = fs.readFileSync("./test/example-language.txt", "utf8");
 	let wrongInput = fs.readFileSync("./test/example-wrong-language.txt", "utf8");
 	let tokenMatchers = [
@@ -28,7 +28,7 @@ describe('LexicalParser', function() {
 	let ignorePattern = '[\n\s \t]+'
 	
 	describe('#constructor()', function() {
-		let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+		let lex = new Lex(input, tokenMatchers, ignorePattern)
 		it('should have a input exactly as passed in the constructor', function (){
 			assert.equal(lex.input, input)
 		})		 	
@@ -39,25 +39,25 @@ describe('LexicalParser', function() {
 
 	describe('#nextToken()', function() {
 		it('should have the first token named', function (){
-			let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+			let lex = new Lex(input, tokenMatchers, ignorePattern)
 			let token = lex.nextToken()
 			assert.equal(token.name, 'block')
 		})
 		it('should match { as the secondToken', function (){
-			let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+			let lex = new Lex(input, tokenMatchers, ignorePattern)
 			lex.nextToken()
 			let token = lex.nextToken()
 			assert.equal(token.lexeme, '{')	 	
 		})
 		it('should match \'int\' as reserved word', function (){
-			let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+			let lex = new Lex(input, tokenMatchers, ignorePattern)
 			lex.nextToken()
 			lex.nextToken()
 			let token = lex.nextToken()
 			assert.equal(token.name, 'int')	 	
 		})
 		it('should match \'a\' as ID', function (){
-			let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+			let lex = new Lex(input, tokenMatchers, ignorePattern)
 			lex.nextToken()
 			lex.nextToken()			
 			lex.nextToken()
@@ -65,7 +65,7 @@ describe('LexicalParser', function() {
 			assert.equal(token.name, 'id')	 	
 		})
 		it('and should have the lexeme as \'a\'', function (){
-			let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+			let lex = new Lex(input, tokenMatchers, ignorePattern)
 			lex.nextToken()
 			lex.nextToken()			
 			lex.nextToken()
@@ -73,7 +73,7 @@ describe('LexicalParser', function() {
 			assert.equal(token.lexeme, 'a')	 	
 		})
 		it('should match string tokens', function (){
-			let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+			let lex = new Lex(input, tokenMatchers, ignorePattern)
 			let token;
 			do {
 				token = lex.nextToken()
@@ -81,7 +81,7 @@ describe('LexicalParser', function() {
 			assert.equal(token.lexeme, '"Hello World"')	 	
 		})
 		it('should give all the tokens, then undefined', function (){
-			let lex = new LexicalParser(input, tokenMatchers, ignorePattern)
+			let lex = new Lex(input, tokenMatchers, ignorePattern)
 			let token
 			do {
 				token = lex.nextToken()
@@ -90,14 +90,14 @@ describe('LexicalParser', function() {
 		})
 
 		it('should give inteiro as `ID`', function (){
-			let lex = new LexicalParser(wrongInput, tokenMatchers, ignorePattern)
+			let lex = new Lex(wrongInput, tokenMatchers, ignorePattern)
 			lex.nextToken()
 			lex.nextToken()
 			let token = lex.nextToken()
 			assert.equal(token.name, "id")	 	
 		})
 		it('and should give int as `int`', function (){
-			let lex = new LexicalParser(wrongInput, tokenMatchers, ignorePattern)
+			let lex = new Lex(wrongInput, tokenMatchers, ignorePattern)
 			lex.nextToken()
 			lex.nextToken()
 			lex.nextToken()
